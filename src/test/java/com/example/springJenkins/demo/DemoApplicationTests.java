153f13c72c42793545af80d41597d0ebf23948c5
pankaj <pankaj.c.chaudhary@oracle.com>
2024-01-28 20:51:05 +0530

    First commit

---
+++ src/test/java/com/example/springJenkins/demo/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springJenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+}
